c955ce99d9fbc998965a0342f04de3be61aa7823
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-04-26 21:45:49 +0900

    first commit

---
+++ MarkdownConverter.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace MarkdownConverter.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        string hello = MarkdownConverter.Class1.HelloWorld();
+        Assert.Equal("Hello World", hello);
+    }
+}
---
+++ MarkdownConverter/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace MarkdownConverter;
+
+public class Class1
+{
+    public static string HelloWorld()
+    {
+        return "Hello World";
+    }
+}
